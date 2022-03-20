ef6ab8f27f75d274fdf665c52845a38b090ee01c
george russ <george_russ@hotmail.co.uk>
2022-03-20 13:31:41 +0000

    Added a few strings to make this easier to understand.

--- BMI-calc.py
+++ BMI-calc.py
@@ -1,14 +1,14 @@
-W=int(input())
-H=float(input())
+W = int(input("Please enter your Weight(kg):"))
+H = float(input("Please enter your Height(Metres):"))
 
 BMI = W / H ** 2
 
 
 if BMI < 18.5:
-    print("Underweight")
+    print("Your BMI is: " + str(BMI) + " You are underweight.")
 elif BMI >= 18.5 and BMI < 25:
-    print("Normal") 
+    print("Your BMI is: " + str(BMI) + " You are at a normal weight.")
 elif BMI >= 25 and BMI < 30:
-    print("Overweight")
-else: 
-    print("Obesity")
+    print("Your BMI is: " + str(BMI) + " You are overweight.")
+else:
+    print("Your BMI is: " + str(BMI) + " You are obese")
